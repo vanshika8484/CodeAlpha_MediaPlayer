f44038d54f9ddaa6d321b5669ffcedd5eb298692
vanshika8484 <vanshikaagarwal8484@gmail.com>
2025-07-08 21:35:41 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Host",
+    artist: "Color Out",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "Roots",
+    artist: "Galdson",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "alone",
+    artist: "Color Out",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "Molotov",
+    artist: "radionowhere",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Nord Groove",
+    artist: "Samurai Soul",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Triplet-Droplet",
+    artist: "Arto Kumanto",
+    img: "music-6",
+    src: "music-6",
+  },
+];
